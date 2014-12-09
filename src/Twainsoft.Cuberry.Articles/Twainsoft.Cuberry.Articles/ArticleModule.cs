6be1dd82305009bf3c2c1b8552a1744cb6a2d4f4
Fabian Deitelhoff <fabian@fabiandeitelhoff.de>
2014-12-09 22:32:33 +0100

    Added hard dependencies to Cuberry and other Microsoft stuff. Implemented first methods for the module.

--- src/Twainsoft.Cuberry.Articles/Twainsoft.Cuberry.Articles/ArticleModule.cs
+++ src/Twainsoft.Cuberry.Articles/Twainsoft.Cuberry.Articles/ArticleModule.cs
@@ -1,12 +1,43 @@
-﻿namespace Twainsoft.Cuberry.Articles
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+using Microsoft.Practices.Unity;
+using P2.Cuberry.Framework.Helper;
+
+namespace Twainsoft.Cuberry.Articles
 {
-    public class ArticleModule
+    public class ArticleModule : IModule, IShowableModule
     {
-        /*public ArticleModule(IUnityContainer container, IRegionManager regionManager)
+        private readonly IUnityContainer container;
+        private readonly IRegionManager regionManager;
+
+        public void Show()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Hide()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string ModuleName
+        {
+            get
+            {
+                return Properties.Settings.Default.ModuleName;
+            }
+        }
+
+        public ArticleModule(IUnityContainer container, IRegionManager regionManager)
         {
             this.container = container;
             this.regionManager = regionManager;
             ModuleRepository.Modules.Add(this.ModuleName, this);
-        }*/
+        }
+
+        public void Initialize()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
