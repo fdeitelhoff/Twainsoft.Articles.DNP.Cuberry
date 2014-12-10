d8e8014822b798913865afecb71d454e0a3d1fd7
Fabian Deitelhoff <fabian@fabiandeitelhoff.de>
2014-12-10 20:15:04 +0100

    Minimal changes within the Articles class (BusinessEntity).

--- src/Twainsoft.Cuberry.Articles/Twainsoft.Cuberry.Articles/BusinessEntities/Article.cs
+++ src/Twainsoft.Cuberry.Articles/Twainsoft.Cuberry.Articles/BusinessEntities/Article.cs
@@ -1,10 +1,24 @@
-﻿namespace Twainsoft.Cuberry.Articles.BusinessEntities
+﻿using P2.Cuberry.Framework.BOLBase;
+using P2.Cuberry.Framework.Helper;
+
+namespace Twainsoft.Cuberry.Articles.BusinessEntities
 {
-    public class Article
+    public class Article : BizObject
     {
         public Article()
         {
             
+        }
+
+        public override P2Validator Validator
+        {
+            get
+            {
+                // TODO: EmptyID ist hier wohl falsch...
+                if (_validator == null)
+                    _validator = new P2Validator(Properties.Settings.Default.ModuleName, this.EntityName, "EmptyID", this);
+                return _validator;
+            }
         }
     }
 }
