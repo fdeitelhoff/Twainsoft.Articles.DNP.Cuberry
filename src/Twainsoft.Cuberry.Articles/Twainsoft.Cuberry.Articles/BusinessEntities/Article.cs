688638a048e2991b1e8692c368474ba91263bbe3
Fabian Deitelhoff <fabian@fabiandeitelhoff.de>
2014-12-12 19:14:15 +0100

    First attribute within the article business entity.

--- src/Twainsoft.Cuberry.Articles/Twainsoft.Cuberry.Articles/BusinessEntities/Article.cs
+++ src/Twainsoft.Cuberry.Articles/Twainsoft.Cuberry.Articles/BusinessEntities/Article.cs
@@ -1,14 +1,18 @@
-﻿using P2.Cuberry.Framework.BOLBase;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using P2.Cuberry.Framework.BOLBase;
 using P2.Cuberry.Framework.Helper;
 using Twainsoft.Cuberry.Articles.Properties;
 
 namespace Twainsoft.Cuberry.Articles.BusinessEntities
 {
-    public class Article : BizObject
+    public class Article : BizObject, INotifyPropertyChanged
     {
+        public int ArticleId { get; protected set; }
+
         public Article()
         {
-            
+            Modified = false;
         }
 
         public override P2Validator Validator
@@ -17,9 +21,17 @@ namespace Twainsoft.Cuberry.Articles.BusinessEntities
             {
                 // TODO: EmptyID ist hier wohl falsch...
                 if (_validator == null)
-                    _validator = new P2Validator(Settings.Default.ModuleName, EntityName, "EmptyID", this);
+                    _validator = new P2Validator(Settings.Default.ModuleName, EntityName, "ArticleID", this);
                 return _validator;
             }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
